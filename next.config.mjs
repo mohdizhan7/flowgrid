df957e8811e43f54126f3bf6e8ae44f98c8c334f
mohdizhan7 <58340944+mohdizhan7@users.noreply.github.com>
2025-08-09 13:43:16 +0530

    refactor: replace deprecated turbo config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,6 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  experimental: { turbo: {} },
+  turbopack: {},
 };
 export default nextConfig;
